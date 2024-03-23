55f35206c1b502d82278656c68eef51bf524374f
gbrielll <105085023+GbrielZanoni@users.noreply.github.com>
2024-03-23 09:07:24 -0300

    gitgnor + comentários

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from discord.ext import commands
 from discord import app_commands 
 import os
 from dotenv import load_dotenv
-from response import get_response  # onde vai ficar o código de resposta
+from response import get_response 
 
 load_dotenv()
 
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-intents.message_content = True  # Necessário para acessar o conteúdo das mensagens
+intents.message_content = True  
 
-# Inicialize o bot com o prefixo "!" e com todos os intents habilitados
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def send_message(message: discord.Message, user_message: str) -> None:
@@ -49,10 +48,9 @@ async def on_message(message: discord.Message):
 
     print(f'[{message.channel}] {message.author}: "{message.content}"')
     await send_message(message, message.content)
-    # Importante! Isso permite que o processamento de comandos continue após este evento.
+
     await bot.process_commands(message)
 
-# Registro dos comandos com barra
 @bot.tree.command(name="teste")
 async def comando(interaction: discord.Interaction):
     await interaction.response.send_message(f"Usuário {interaction.user.mention} {interaction.user.id} !!!", ephemeral=True)
