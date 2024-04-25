50b0d73a2d1068ae25b400edd706f8161a7d9d3e
gbrielll <105085023+GbrielZanoni@users.noreply.github.com>
2024-04-25 17:19:57 -0300

    mudança final

--- botdiscordunifil/main.py
+++ botdiscordunifil/main.py
@@ -1,10 +1,6 @@
 from typing import Final
 import discord
 from discord.ext import commands
-from discord_interactions import InteractionClient
-from discord_slash.context import MenuContext
-from discord_slash.model import ContextMenuType
-
 import os
 from dotenv import load_dotenv
 
@@ -16,14 +12,13 @@ intents = discord.Intents.default()
 intents.message_content = True  
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-slash = InteractionClient(bot, test_mode=True)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now running!')
 
-@slash.command(description="Envia um embed simples")
-async def embed(ctx: MenuContext):
+@bot.command(description="Envia um embed simples")
+async def embed(ctx: commands.Context):
     embed = discord.Embed(
         colour = discord.Colour.dark_teal(),
         description="teste",
@@ -34,16 +29,12 @@ async def embed(ctx: MenuContext):
     
     await ctx.send(embed=embed)
 
-@slash.command(description="Um comando de teste")  
-async def teste(ctx: MenuContext):  
+@bot.command(description="teste")  
+async def teste(ctx: commands.Context):
     await ctx.send(f"Usuário {ctx.author.mention} {ctx.author.id} !!!", ephemeral=True)
 
-@slash.command(description="Soma dois números")
-async def soma(ctx: MenuContext, arg1: int, arg2: int):
+@bot.command(description="Soma dois números")
+async def soma(ctx: commands.Context, arg1: int, arg2: int):
     await ctx.send(f"A soma é de: {arg1 + arg2} !!!", ephemeral=True)
 
-def main():
-    bot.run(TOKEN)
-
-if __name__ == '__main__':
-    main()
+bot.run(TOKEN)
