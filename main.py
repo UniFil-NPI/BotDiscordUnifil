89f4c29f701d08089d6db7e17c0453222802db4c
gbrielll <105085023+GbrielZanoni@users.noreply.github.com>
2024-04-25 14:42:39 -0300

    discord_slash no poetry

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Final
 import discord
 from discord.ext import commands
-from discord import app_commands 
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
 import os
 from dotenv import load_dotenv
 
@@ -13,24 +14,51 @@ intents = discord.Intents.default()
 intents.message_content = True  
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now running!')
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
 
-@bot.tree.command(name="teste")
-async def comando(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Usuário {interaction.user.mention} {interaction.user.id} !!!", ephemeral=True)
+@slash.slash(name="embed",
+             description="Envia um embed simples",
+             guild_ids=None) 
+async def embed(ctx: SlashContext):
+    embed = discord.Embed(
+        colour = discord.Colour.dark_teal(),
+        description="teste",
+        title="título", 
+    )
+    embed.set_footer(text="111")
+    embed.set_author(name="Bot")
+    
+    await ctx.send(embed=embed)
 
-@bot.tree.command(name="soma")
-@app_commands.describe(arg1="primeiro número", arg2="segundo número")
-async def soma(interaction: discord.Interaction, arg1: int, arg2: int):
-    await interaction.response.send_message(f"A soma é de: {arg1 + arg2} !!!", ephemeral=True)
+@slash.slash(name="teste",
+             description="Um comando de teste",
+             guild_ids=None)  
+async def teste(ctx: SlashContext):
+    await ctx.send(f"Usuário {ctx.author.mention} {ctx.author.id} !!!", ephemeral=True)
+
+@slash.slash(name="soma",
+             description="Soma dois números",
+             guild_ids=None, 
+             options=[
+                 create_option(
+                     name="arg1",
+                     description="Primeiro número",
+                     option_type=4,
+                     required=True
+                 ),
+                 create_option(
+                     name="arg2",
+                     description="Segundo número",
+                     option_type=4,
+                     required=True
+                 )
+             ])
+async def soma(ctx: SlashContext, arg1: int, arg2: int):
+    await ctx.send(f"A soma é de: {arg1 + arg2} !!!", ephemeral=True)
 
 def main():
     bot.run(TOKEN)
