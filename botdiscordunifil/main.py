cc2daa891173aeb55a208070b23e50fdbca53466
gb <105085023+GbrielZanoni@users.noreply.github.com>
2024-04-26 19:24:02 -0300

    Update main.py

--- botdiscordunifil/main.py
+++ botdiscordunifil/main.py
@@ -1,11 +1,19 @@
 from typing import Final
+
 import discord
+from discord import Embed
 from discord.ext import commands
+from discord import app_commands 
+import interactions
+import time
+
 import os
+import datetime
+
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# install discord-py-interactions
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
@@ -15,26 +23,49 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} is now running!')
+    print(f'{bot.user} está rodando!')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Foram carregados {len(synced)} comando(s)")
+    except Exception as e:
+        print(e)
 
-@bot.command(description="Envia um embed simples")
-async def embed(ctx: commands.Context):
-    embed = discord.Embed(
-        colour = discord.Colour.dark_teal(),
-        description="teste",
-        title="título", 
-    )
-    embed.set_footer(text="111")
-    embed.set_author(name="Bot")
-    
-    await ctx.send(embed=embed)
+@bot.tree.command(name="embed")
+async def embed_command(interaction: discord.Interaction):
+    user = interaction.user
+    embed = Embed(title="Embed", color=0x00ff00)
+    embed.add_field(name="1", value="1", inline=False)
+    embed.add_field(name="2", value="2", inline=True)
+    embed.set_footer(text="Fim")
+    embed.set_author(name=user.display_name, icon_url=user.display_avatar.url)
 
-@bot.command(description="teste")  
-async def teste(ctx: commands.Context):
-    await ctx.send(f"Usuário {ctx.author.mention} {ctx.author.id} !!!", ephemeral=True)
+    await interaction.response.send_message(embed=embed, ephemeral=False)
 
-@bot.command(description="Soma dois números")
-async def soma(ctx: commands.Context, arg1: int, arg2: int):
-    await ctx.send(f"A soma é de: {arg1 + arg2} !!!", ephemeral=True)
+@bot.tree.command(name="calendario")
+async def calendario_command(interaction: discord.Interaction):
+    user = interaction.user
+    embed = Embed(title="Calendário de Atividades", color=0x3498db)
 
-bot.run(TOKEN)
+    datas = {
+        "Programação": datetime.datetime(2024, 5, 5, 15, 30),
+        "Back End": datetime.datetime(2024, 5, 12, 14, 00),
+        "Front End": datetime.datetime(2024, 5, 19, 16, 45)
+    }
+
+    for atividade, data in datas.items():
+        timestamp_data = f"<t:{int(data.timestamp())}:D>"  
+        timestamp_relative = f"<t:{int(data.timestamp())}:R>" 
+
+        timestamp_combined = f"{timestamp_data} {timestamp_relative}"
+
+        embed.add_field(name=atividade, value=f"{timestamp_combined}", inline=False)
+
+    embed.set_author(name=user.display_name, icon_url=user.display_avatar.url)
+
+    await interaction.response.send_message(embed=embed, ephemeral=False)
+
+def main():
+    bot.run(TOKEN)
+
+if __name__ == '__main__':
+    main()
